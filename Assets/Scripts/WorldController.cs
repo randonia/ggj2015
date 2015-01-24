12fac77f50294c3556fd2369b9e74d040831c929
Ryan Andonian <ryanandonian@gmail.com>
2015-01-23 18:40:08 -0800

    First commit! World controller is there. Set up Visual Studio to use Unix line endings, so all code I write will have the proper line endings, but the built-in code is all CRLF

---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Controls the grid
+/// </summary>
+public class WorldController : MonoBehaviour {
+    public const float GLOBAL_Z = 1.0f;
+
+    public GameObject PREFAB_TILE1;
+
+    public GameObject TileContainer;
+
+    public Vector2 worldSize;
+
+    /// <summary>
+    /// THE GRID. X,Y based. Make sure to round.
+    /// </summary>
+    public Dictionary<int, Dictionary<int, GameObject>> THE_GRID;
+
+	// Use this for initialization
+	void Start () {
+        
+	    // Instantiate the world according to worldSize
+        for (int x = (int)-worldSize.x; x < worldSize.x; ++x)
+        {
+            for (int y = (int)-worldSize.y; y < worldSize.y; ++y)
+            {
+                GameObject newTile = GameObject.Instantiate(PREFAB_TILE1) as GameObject;
+                newTile.transform.position = new Vector3(x, y, GLOBAL_Z);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
