a51df7f3c6c2f9a000464bc7dda1aa3f3012375b
Ryan Andonian <ryanandonian@gmail.com>
2015-01-25 00:00:06 -0800

    Added camera controller class. Needs twerk still.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject GO_Player;
+
+    public float CameraSpeed = 1.0f;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 delta = (GO_Player.transform.position - transform.position).normalized;
+        delta.z = 0.0f;
+        transform.Translate(delta * Time.deltaTime * CameraSpeed);
+    }
+}
